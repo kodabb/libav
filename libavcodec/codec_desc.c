f46822bc278685f2845869f239a746269755aa69
Paul B Mahol <onemda@gmail.com>
2012-03-19 02:54:19 +0000

    LucasArts SMUSH SANM video decoder
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavcodec/codec_desc.c
+++ libavcodec/codec_desc.c
@@ -1092,6 +1092,13 @@ static const AVCodecDescriptor codec_descriptors[] = {
         .long_name = NULL_IF_CONFIG_SMALL("Mirillis FIC"),
         .props     = AV_CODEC_PROP_LOSSY,
     },
+    {
+        .id        = AV_CODEC_ID_SANM,
+        .type      = AVMEDIA_TYPE_VIDEO,
+        .name      = "sanm",
+        .long_name = NULL_IF_CONFIG_SMALL("LucasArts SANM video"),
+        .props     = AV_CODEC_PROP_LOSSY,
+    },
 
     /* image codecs */
     {
