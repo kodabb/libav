082bed6eddd17ffbddd9066d48a44fa3067aba42
Anton Khirnov <anton@khirnov.net>
2013-05-07 07:20:32 +0200

    vorbisenc: convert to new channel layout API

--- libavcodec/vorbisenc.c
+++ libavcodec/vorbisenc.c
@@ -242,7 +242,7 @@ static int create_vorbis_context(vorbis_enc_context *venc,
     vorbis_enc_mapping *mc;
     int i, book, ret;
 
-    venc->channels    = avctx->channels;
+    venc->channels    = avctx->ch_layout.nb_channels;
     venc->sample_rate = avctx->sample_rate;
     venc->log2_blocksize[0] = venc->log2_blocksize[1] = 11;
 
@@ -1166,7 +1166,7 @@ static av_cold int vorbis_encode_init(AVCodecContext *avctx)
     vorbis_enc_context *venc = avctx->priv_data;
     int ret;
 
-    if (avctx->channels != 2) {
+    if (avctx->ch_layout.nb_channels != 2) {
         av_log(avctx, AV_LOG_ERROR, "Current Libav Vorbis encoder only supports 2 channels.\n");
         return -1;
     }
