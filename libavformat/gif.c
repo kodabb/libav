9e4b6291397c9a8848129af86f59870fb1230bb6
Michael Niedermayer <michaelni@gmx.at>
2013-05-23 15:51:11 +0200

    lavf/gif: remove unneeded doxygen comment

--- libavformat/gif.c
+++ libavformat/gif.c
@@ -65,7 +65,7 @@ static int gif_image_write_header(AVIOContext *pb, int width, int height,
 }
 
 typedef struct {
-    AVClass *class;         /** Class for private options. */
+    AVClass *class;
     int loop;
     int last_delay;
     AVPacket prev_pkt;
