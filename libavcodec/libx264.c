350f165907b3148f300838e24e2e24dee1acc47a
Vittorio Giovara <vittorio.giovara@gmail.com>
2013-07-12 14:23:18 +0200

    libx264: move init and close functions at the top of the file

--- libavcodec/libx264.c
+++ libavcodec/libx264.c
@@ -94,125 +94,6 @@ static void X264_log(void *p, int level, const char *fmt, va_list args)
     av_vlog(p, level_map[level], fmt, args);
 }
 
-
-static int encode_nals(AVCodecContext *ctx, AVPacket *pkt,
-                       x264_nal_t *nals, int nnal)
-{
-    X264Context *x4 = ctx->priv_data;
-    uint8_t *p;
-    int i, size = x4->sei_size, ret;
-
-    if (!nnal)
-        return 0;
-
-    for (i = 0; i < nnal; i++)
-        size += nals[i].i_payload;
-
-    if ((ret = ff_alloc_packet(pkt, size)) < 0)
-        return ret;
-
-    p = pkt->data;
-
-    /* Write the SEI as part of the first frame. */
-    if (x4->sei_size > 0 && nnal > 0) {
-        memcpy(p, x4->sei, x4->sei_size);
-        p += x4->sei_size;
-        x4->sei_size = 0;
-    }
-
-    for (i = 0; i < nnal; i++){
-        memcpy(p, nals[i].p_payload, nals[i].i_payload);
-        p += nals[i].i_payload;
-    }
-
-    return 1;
-}
-
-static int X264_frame(AVCodecContext *ctx, AVPacket *pkt, const AVFrame *frame,
-                      int *got_packet)
-{
-    X264Context *x4 = ctx->priv_data;
-    x264_nal_t *nal;
-    int nnal, i, ret;
-    x264_picture_t pic_out;
-
-    x264_picture_init( &x4->pic );
-    x4->pic.img.i_csp   = x4->params.i_csp;
-    if (x264_bit_depth > 8)
-        x4->pic.img.i_csp |= X264_CSP_HIGH_DEPTH;
-    x4->pic.img.i_plane = 3;
-
-    if (frame) {
-        for (i = 0; i < 3; i++) {
-            x4->pic.img.plane[i]    = frame->data[i];
-            x4->pic.img.i_stride[i] = frame->linesize[i];
-        }
-
-        x4->pic.i_pts  = frame->pts;
-        x4->pic.i_type =
-            frame->pict_type == AV_PICTURE_TYPE_I ? X264_TYPE_KEYFRAME :
-            frame->pict_type == AV_PICTURE_TYPE_P ? X264_TYPE_P :
-            frame->pict_type == AV_PICTURE_TYPE_B ? X264_TYPE_B :
-                                            X264_TYPE_AUTO;
-        if (x4->params.b_tff != frame->top_field_first) {
-            x4->params.b_tff = frame->top_field_first;
-            x264_encoder_reconfig(x4->enc, &x4->params);
-        }
-        if (x4->params.vui.i_sar_height != ctx->sample_aspect_ratio.den ||
-            x4->params.vui.i_sar_width  != ctx->sample_aspect_ratio.num) {
-            x4->params.vui.i_sar_height = ctx->sample_aspect_ratio.den;
-            x4->params.vui.i_sar_width  = ctx->sample_aspect_ratio.num;
-            x264_encoder_reconfig(x4->enc, &x4->params);
-        }
-    }
-
-    do {
-        if (x264_encoder_encode(x4->enc, &nal, &nnal, frame? &x4->pic: NULL, &pic_out) < 0)
-            return -1;
-
-        ret = encode_nals(ctx, pkt, nal, nnal);
-        if (ret < 0)
-            return -1;
-    } while (!ret && !frame && x264_encoder_delayed_frames(x4->enc));
-
-    pkt->pts = pic_out.i_pts;
-    pkt->dts = pic_out.i_dts;
-
-    switch (pic_out.i_type) {
-    case X264_TYPE_IDR:
-    case X264_TYPE_I:
-        x4->out_pic.pict_type = AV_PICTURE_TYPE_I;
-        break;
-    case X264_TYPE_P:
-        x4->out_pic.pict_type = AV_PICTURE_TYPE_P;
-        break;
-    case X264_TYPE_B:
-    case X264_TYPE_BREF:
-        x4->out_pic.pict_type = AV_PICTURE_TYPE_B;
-        break;
-    }
-
-    pkt->flags |= AV_PKT_FLAG_KEY*pic_out.b_keyframe;
-    if (ret)
-        x4->out_pic.quality = (pic_out.i_qpplus1 - 1) * FF_QP2LAMBDA;
-
-    *got_packet = ret;
-    return 0;
-}
-
-static av_cold int X264_close(AVCodecContext *avctx)
-{
-    X264Context *x4 = avctx->priv_data;
-
-    av_freep(&avctx->extradata);
-    av_free(x4->sei);
-
-    if (x4->enc)
-        x264_encoder_close(x4->enc);
-
-    return 0;
-}
-
 static int convert_pix_fmt(enum AVPixelFormat pix_fmt)
 {
     switch (pix_fmt) {
@@ -472,6 +353,123 @@ static av_cold int X264_init(AVCodecContext *avctx)
         avctx->extradata_size = p - avctx->extradata;
     }
 
+    return 0;
+}
+
+static av_cold int X264_close(AVCodecContext *avctx)
+{
+    X264Context *x4 = avctx->priv_data;
+
+    av_freep(&avctx->extradata);
+    av_free(x4->sei);
+
+    if (x4->enc)
+        x264_encoder_close(x4->enc);
+
+    return 0;
+}
+
+static int encode_nals(AVCodecContext *ctx, AVPacket *pkt,
+                       x264_nal_t *nals, int nnal)
+{
+    X264Context *x4 = ctx->priv_data;
+    uint8_t *p;
+    int i, size = x4->sei_size, ret;
+
+    if (!nnal)
+        return 0;
+
+    for (i = 0; i < nnal; i++)
+        size += nals[i].i_payload;
+
+    if ((ret = ff_alloc_packet(pkt, size)) < 0)
+        return ret;
+
+    p = pkt->data;
+
+    /* Write the SEI as part of the first frame. */
+    if (x4->sei_size > 0 && nnal > 0) {
+        memcpy(p, x4->sei, x4->sei_size);
+        p += x4->sei_size;
+        x4->sei_size = 0;
+    }
+
+    for (i = 0; i < nnal; i++){
+        memcpy(p, nals[i].p_payload, nals[i].i_payload);
+        p += nals[i].i_payload;
+    }
+
+    return 1;
+}
+
+static int X264_frame(AVCodecContext *ctx, AVPacket *pkt, const AVFrame *frame,
+                      int *got_packet)
+{
+    X264Context *x4 = ctx->priv_data;
+    x264_nal_t *nal;
+    int nnal, i, ret;
+    x264_picture_t pic_out;
+
+    x4->pic.img.i_csp   = x4->params.i_csp;
+    if (x264_bit_depth > 8)
+        x4->pic.img.i_csp |= X264_CSP_HIGH_DEPTH;
+    x4->pic.img.i_plane = 3;
+
+    if (frame) {
+        for (i = 0; i < 3; i++) {
+            x4->pic.img.plane[i]    = frame->data[i];
+            x4->pic.img.i_stride[i] = frame->linesize[i];
+        }
+
+        x4->pic.i_pts  = frame->pts;
+        x4->pic.i_type =
+            frame->pict_type == AV_PICTURE_TYPE_I ? X264_TYPE_KEYFRAME :
+            frame->pict_type == AV_PICTURE_TYPE_P ? X264_TYPE_P :
+            frame->pict_type == AV_PICTURE_TYPE_B ? X264_TYPE_B :
+                                            X264_TYPE_AUTO;
+        if (x4->params.b_tff != frame->top_field_first) {
+            x4->params.b_tff = frame->top_field_first;
+            x264_encoder_reconfig(x4->enc, &x4->params);
+        }
+        if (x4->params.vui.i_sar_height != ctx->sample_aspect_ratio.den ||
+            x4->params.vui.i_sar_width  != ctx->sample_aspect_ratio.num) {
+            x4->params.vui.i_sar_height = ctx->sample_aspect_ratio.den;
+            x4->params.vui.i_sar_width  = ctx->sample_aspect_ratio.num;
+            x264_encoder_reconfig(x4->enc, &x4->params);
+        }
+    }
+
+    do {
+        if (x264_encoder_encode(x4->enc, &nal, &nnal, frame? &x4->pic: NULL, &pic_out) < 0)
+            return -1;
+
+        ret = encode_nals(ctx, pkt, nal, nnal);
+        if (ret < 0)
+            return -1;
+    } while (!ret && !frame && x264_encoder_delayed_frames(x4->enc));
+
+    pkt->pts = pic_out.i_pts;
+    pkt->dts = pic_out.i_dts;
+
+    switch (pic_out.i_type) {
+    case X264_TYPE_IDR:
+    case X264_TYPE_I:
+        x4->out_pic.pict_type = AV_PICTURE_TYPE_I;
+        break;
+    case X264_TYPE_P:
+        x4->out_pic.pict_type = AV_PICTURE_TYPE_P;
+        break;
+    case X264_TYPE_B:
+    case X264_TYPE_BREF:
+        x4->out_pic.pict_type = AV_PICTURE_TYPE_B;
+        break;
+    }
+
+    pkt->flags |= AV_PKT_FLAG_KEY*pic_out.b_keyframe;
+    if (ret)
+        x4->out_pic.quality = (pic_out.i_qpplus1 - 1) * FF_QP2LAMBDA;
+
+    *got_packet = ret;
     return 0;
 }
 
