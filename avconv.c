3291cee3db9d3ca6d524e1d6902a643f1b086007
Vittorio Giovara <vittorio.giovara@gmail.com>
2013-11-26 17:32:26 +0100

    avconv: convert hint for strict experimental message

--- avconv.c
+++ avconv.c
@@ -238,11 +238,11 @@ static void abort_codec_experimental(AVCodec *c, int encoder)
     const char *codec_string = encoder ? "encoder" : "decoder";
     AVCodec *codec;
     av_log(NULL, AV_LOG_FATAL, "%s '%s' is experimental and might produce bad "
-            "results.\nAdd '-strict experimental' if you want to use it.\n",
-            codec_string, c->name);
+            "results.\n", codec_string, c->name);
+    av_log(NULL, AV_LOG_HINT, "Add '-strict experimental' if you want to use it.\n");
     codec = encoder ? avcodec_find_encoder(c->id) : avcodec_find_decoder(c->id);
     if (!(codec->capabilities & CODEC_CAP_EXPERIMENTAL))
-        av_log(NULL, AV_LOG_FATAL, "Or use the non experimental %s '%s'.\n",
+        av_log(NULL, AV_LOG_HINT, "Or use the non experimental %s '%s'.\n",
                codec_string, codec->name);
     exit_program(1);
 }
