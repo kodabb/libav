1156fbf4c95ffed57fb1425ca956b8ae241c8c7e
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-04-15 04:01:44 +0200

    hevc: parse display orientation SEI message

--- libavcodec/hevc.c
+++ libavcodec/hevc.c
@@ -25,6 +25,7 @@
 
 #include "libavutil/attributes.h"
 #include "libavutil/common.h"
+#include "libavutil/display.h"
 #include "libavutil/internal.h"
 #include "libavutil/md5.h"
 #include "libavutil/opt.h"
@@ -2454,6 +2455,22 @@ static int set_side_data(HEVCContext *s)
 
         if (s->content_interpretation_type == 2)
             stereo->flags = AV_STEREO3D_FLAG_INVERT;
+    }
+
+    if (s->sei_display_orientation_present &&
+        (s->sei_anticlockwise_rotation || s->sei_hflip || s->sei_vflip)) {
+        int32_t display_matrix[9];
+        double angle = s->sei_anticlockwise_rotation * 360 / (1 << 16);
+        AVFrameSideData *rotation = av_frame_new_side_data(out,
+                                                           AV_FRAME_DATA_DISPLAYMATRIX,
+                                                           sizeof(display_matrix));
+        if (!rotation) {
+            av_free(rotation);
+            return AVERROR(ENOMEM);
+        }
+        av_display_rotation_set(display_matrix, angle);
+        av_display_matrix_flip(display_matrix, s->sei_vflip, s->sei_hflip);
+        memcpy(rotation->data, display_matrix, sizeof(display_matrix));
     }
 
     return 0;
