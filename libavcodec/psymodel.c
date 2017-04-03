caa09325ef16971740e0b4b6adaed5c3aa3d3d08
Vittorio Giovara <vittorio.giovara@gmail.com>
2017-04-03 14:12:57 +0200

    psymodel: convert to new channel layout API

--- libavcodec/psymodel.c
+++ libavcodec/psymodel.c
@@ -35,7 +35,7 @@ av_cold int ff_psy_init(FFPsyContext *ctx, AVCodecContext *avctx, int num_lens,
     int i, j, k = 0;
 
     ctx->avctx = avctx;
-    ctx->ch        = av_mallocz(sizeof(ctx->ch[0]) * avctx->channels * 2);
+    ctx->ch        = av_mallocz(sizeof(ctx->ch[0]) * avctx->ch_layout.nb_channels * 2);
     ctx->group     = av_mallocz(sizeof(ctx->group[0]) * num_groups);
     ctx->bands     = av_malloc (sizeof(ctx->bands[0])     * num_lens);
     ctx->num_bands = av_malloc (sizeof(ctx->num_bands[0]) * num_lens);
@@ -116,12 +116,12 @@ av_cold struct FFPsyPreprocessContext* ff_psy_preprocess_init(AVCodecContext *av
                                              FF_FILTER_MODE_LOWPASS, FILT_ORDER,
                                              cutoff_coeff, 0.0, 0.0);
     if (ctx->fcoeffs) {
-        ctx->fstate = av_mallocz(sizeof(ctx->fstate[0]) * avctx->channels);
+        ctx->fstate = av_mallocz(sizeof(ctx->fstate[0]) * avctx->ch_layout.nb_channels);
         if (!ctx->fstate) {
             av_free(ctx);
             return NULL;
         }
-        for (i = 0; i < avctx->channels; i++)
+        for (i = 0; i < avctx->ch_layout.nb_channels; i++)
             ctx->fstate[i] = ff_iir_filter_init_state(FILT_ORDER);
     }
     return ctx;
@@ -144,7 +144,7 @@ av_cold void ff_psy_preprocess_end(struct FFPsyPreprocessContext *ctx)
     int i;
     ff_iir_filter_free_coeffs(ctx->fcoeffs);
     if (ctx->fstate)
-        for (i = 0; i < ctx->avctx->channels; i++)
+        for (i = 0; i < ctx->avctx->ch_layout.nb_channels; i++)
             ff_iir_filter_free_state(ctx->fstate[i]);
     av_freep(&ctx->fstate);
     av_free(ctx);
